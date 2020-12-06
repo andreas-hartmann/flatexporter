1c631953ffa5a4eb2bb89d78ae030ab93257fb1e
Andreas Hartmann <andreas@ohok.org>
2020-12-06 21:43:40 +0100

    Version 1.1.

--- flatexporter.sh
+++ flatexporter.sh
@@ -14,17 +14,45 @@
 #                                        888
 #
 # Exports the contents of a flatex depot as csv.
+#
+# Requires curl and jq or gpg. If jq is not found in the path, it will be downloaded to /tmp.
+# Gpg is required to verify the signature of the downloaded file. It's only needed if jq isn't already installed.
+#
 # Usage: ./flatexporter.sh username password
+#
+# Version: 1.1
 #
 # Author: Andreas Hartmann
 # https://github.com/Yugen42/flatexporter
 #######################################
 
+# Download jq if it's not in the path.
+jqpath=$(which jq)
+if ! [ -x "$jqpath" ] ; then
+	JQ_VERSION=1.5
+
+	curl -Ls https://raw.githubusercontent.com/stedolan/jq/master/sig/jq-release.key --output /tmp/jq-release.key
+	curl -Ls https://raw.githubusercontent.com/stedolan/jq/master/sig/v${JQ_VERSION}/jq-linux64.asc --output /tmp/jq-linux64.asc
+	curl -Ls https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64 --output /tmp/jq-linux64
+	gpg --import /tmp/jq-release.key
+	gpg --verify /tmp/jq-linux64.asc /tmp/jq-linux64
+
+	if ! [ $? -eq 0 ] ; then
+		exit $?
+	fi
+
+	chmod +x /tmp/jq-linux64
+
+	jq () {
+		/tmp/jq-linux64 $1 $2
+	}
+fi
+
 # Log in and store cookies in a temporary file.
 curl -sLc /tmp/cookies --post302 \
 	'https://www.flatex.de/sso'\
 	--data-raw "tx_flatexaccounts_singlesignonbanking%5Buname_app%5D=$1&tx_flatexaccounts_singlesignonbanking%5Bpassword_app%5D=$2"\
-	--compressed > /dev/null
+	--compressed --output /dev/null
 # Extract the sessionid.
 jsessionid=$(cat /tmp/cookies | grep -oP '(?<=JSESSIONID).*' | xargs)
 
